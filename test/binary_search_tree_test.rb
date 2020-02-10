e97ff84a0c8b2503c44e2aa4c968dd748db740de
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-09 17:37:54 -0700

    Add insert and depth of method to BinarySearchTree class and test they work

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/binary_search_tree'
+require './lib/node'
 
 class BinarySearchTreeTest < Minitest::Test
   def setup
@@ -9,5 +10,18 @@ class BinarySearchTreeTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of BinarySearchTree, @tree
+  end
+
+  def test_insert_creates_node
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 61, @tree.root_node.score
+    assert_equal "Bill & Ted's Excellent Adventure", @tree.root_node.movie
+  end
+
+  def test_insert_returns_depth_of_node
+    assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
   end
 end
